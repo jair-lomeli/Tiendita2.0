f2cbbe1ac983a3a4d18df5e1c89304031a6758e0
jair-lomeli <rjlomeli17@gmail.com>
2025-01-21 14:58:26 -0600

    Realice una funcion getoneuser en el modelo users

--- server/models/ticket.js
+++ server/models/ticket.js
@@ -2,4 +2,13 @@
 
 module.exports = function(Ticket) {
 
+	Ticket.CreateTicket =  function(ticket, callback){
+	
+	Ticket.create(ticket, (err, newTicket)=> {
+		if (err) return callback(err)
+		else return callback(newTicket)
+	})
+
+	};
+
 };
--- server/models/users.js
+++ server/models/users.js
@@ -2,4 +2,22 @@
 
 module.exports = function(Users) {
 
+Users.GetoneUsers = function(callback){
+	Users.find({},(err, User)=>{
+		if(err){
+			console.error('Error', err);
+		} else {
+			console.log ('Usuario encontrado',user);
+		}
+	});
+ }
+
+ Users.GetallUsers = function(callback){
+
+	Users.find({}, (err, users)=> {
+		console.log(users)
+		if (err) return callback(err)
+		else return callback(users)
+	})
+}
 };
